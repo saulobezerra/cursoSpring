74f1868bb65f234f4cc56ee52b255dc1432322cd
Saulo Bezerra <sauloalves89@gmail.com>
2020-03-22 11:54:40 -0300

    Projeto criado

---
+++ src/main/java/com/curso/spring/ionic/CursoSpingIonicApplication.java
@@ -0,0 +1,13 @@
+package com.curso.spring.ionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpingIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpingIonicApplication.class, args);
+	}
+
+}
