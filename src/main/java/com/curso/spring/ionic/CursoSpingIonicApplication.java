3fafccdef2f7a758fd3751aa687d359f8d6e7dd8
Saulo Bezerra <sauloalves89@gmail.com>
2020-03-29 15:37:50 -0300

    Cliente, TipoCliente, Telefones e Enderecos

--- src/main/java/com/curso/spring/ionic/CursoSpingIonicApplication.java
+++ src/main/java/com/curso/spring/ionic/CursoSpingIonicApplication.java
@@ -1,6 +1,5 @@
 package com.curso.spring.ionic;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.curso.spring.ionic.domain.Categoria;
 import com.curso.spring.ionic.domain.Cidade;
+import com.curso.spring.ionic.domain.Cliente;
+import com.curso.spring.ionic.domain.Endereco;
 import com.curso.spring.ionic.domain.Estado;
 import com.curso.spring.ionic.domain.Produto;
+import com.curso.spring.ionic.domain.enums.TipoCliente;
 import com.curso.spring.ionic.repositories.CategoriaRepository;
 import com.curso.spring.ionic.repositories.CidadeRepository;
+import com.curso.spring.ionic.repositories.ClienteRepository;
+import com.curso.spring.ionic.repositories.EnderecoRepository;
 import com.curso.spring.ionic.repositories.EstadoRepository;
 import com.curso.spring.ionic.repositories.ProdutoRepository;
 
@@ -31,6 +35,12 @@ public class CursoSpingIonicApplication implements CommandLineRunner {
 	
 	@Autowired
 	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	
 	public static void main(String[] args) {
@@ -69,7 +79,17 @@ public class CursoSpingIonicApplication implements CommandLineRunner {
 		
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
-				
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "363789912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 	}
 	
 }
